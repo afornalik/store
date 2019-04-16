bd38387a9044c186bf21876a8c2c12128302db44
Andrzejf <afornalik2@gmail.com>
2019-04-17 00:50:01 +0200

    saveEmployee method update

--- src/main/java/store/controller/EmployeeController.java
+++ src/main/java/store/controller/EmployeeController.java
@@ -3,14 +3,21 @@ package store.controller;
 import javax.servlet.ServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import store.model.Employee;
 
 @Controller
 @RequestMapping(path = "/store")
 public class EmployeeController {
 
-	public String saveEmployee(ServletRequest request) {
+	public String saveEmployee(Model model,Employee employee) {
+		System.out.println("first name : "+ employee.getFirstName());
+		System.out.println("last name : "+ employee.getLastName());
+		System.out.println("salary : "+ employee.getSalary());
 		
+		model.addAttribute(employee);
 		return "display";
 	}
 
